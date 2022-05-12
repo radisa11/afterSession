787b61ba1a42ad028b1d38aee44e99095dfa8ceb
radisa11 <zlatic66@.com>
2022-05-12 13:57:37 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using afterSession.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace afterSession.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if(HttpContext.Session.GetInt32("Count") == null)
+            {
+                HttpContext.Session.SetInt32("Count", 1);
+            }else{
+                int? count = (int)HttpContext.Session.GetInt32("Count");
+                HttpContext.Session.SetInt32("Count",(int)++count);
+            }
+            ViewBag.Count = HttpContext.Session.GetInt32("Count");
+            return View();
+        }
+
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
